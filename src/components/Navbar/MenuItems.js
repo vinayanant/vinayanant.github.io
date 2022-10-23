c2cea1939761b9c026b96b6c977eb78da973a104
Vinay Anant <47994405+vinayanant@users.noreply.github.com>
2022-10-22 23:51:47 -0500

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'Contact-us',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links-mobile'
+
+    },
+]
